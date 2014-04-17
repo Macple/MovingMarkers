a5b0937feddfd1b0bbe93ed0e4b1f490193228fd
Maciej Plewko <maciej.plewko@gmail.com>
2014-04-17 17:06:07 +0200

    [MOD] MapActivity small code refactor.

--- app/src/main/java/com/example/myapplication2/app/MapActivity.java
+++ app/src/main/java/com/example/myapplication2/app/MapActivity.java
@@ -1,17 +1,13 @@
 package com.example.myapplication2.app;
 
-import android.animation.IntEvaluator;
-import android.animation.ValueAnimator;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.SystemClock;
 import android.util.Log;
 import android.view.View;
-import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.Interpolator;
 import android.view.animation.LinearInterpolator;
 import android.widget.Toast;
@@ -23,12 +19,9 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
-import com.google.android.gms.maps.model.Circle;
-import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.model.Polygon;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -48,7 +41,6 @@ public class MapActivity extends Activity implements View.OnClickListener {
     private List<Marker> movingMarkers = new ArrayList<Marker>();
     private boolean mapCreated = false;
     private Boolean isMarkerComingBack = false;
-    private Boolean isMarkerMoving = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,44 +68,33 @@ public class MapActivity extends Activity implements View.OnClickListener {
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.move_button:{
-                //if (!isMarkerMoving) {
-
-                    //int markersNumber = movingMarkers.size();
-
-                    if (!isMarkerComingBack) {
-                        isMarkerMoving = true;
-                            animateMarker(movingMarkers.get(0), route.get(1), false);
-                            animateMarker(movingMarkers.get(1), route.get(2), false);
-                            animateMarker(movingMarkers.get(2), route.get(3), false);
-                            animateMarker(movingMarkers.get(3), route.get(4), false);
-                            animateMarker(movingMarkers.get(4), route.get(0), false);
-                        /*moveMarkers(0,1, false);
-                        moveMarkers(1,2, false);
-                        moveMarkers(2,3, false);
-                        moveMarkers(3,4, false);
-                        moveMarkers(4,0, false);*/
-                            isMarkerComingBack = true;
-                        isMarkerMoving = false;
-                    } else {
-                        isMarkerMoving = true;
-                            animateMarker(movingMarkers.get(0), route.get(0), false);
-                            animateMarker(movingMarkers.get(1), route.get(1), false);
-                            animateMarker(movingMarkers.get(2), route.get(2), false);
-                            animateMarker(movingMarkers.get(3), route.get(3), false);
-                            animateMarker(movingMarkers.get(4), route.get(4), false);
-                        /*moveMarkers(0,0, false);
-                        moveMarkers(1,1, false);
-                        moveMarkers(2,2, false);
-                        moveMarkers(3,3, false);
-                        moveMarkers(4,4, false);*/
-                        isMarkerComingBack = false;
-                        isMarkerMoving = false;
-                    }
-                }
+                setMarkersAnimation();
+            }
             break;
         }
     }
 
+    private void setMarkersAnimation() {
+        if (!isMarkerComingBack) {
+            for (int i = 0; i < movingMarkers.size(); i++) {
+                if (i < route.size() - 1) {
+                    animateMarker(movingMarkers.get(i), route.get(i + 1), false);
+                } else {
+                    animateMarker(movingMarkers.get(i), route.get(0), false);
+                }
+            }
+            isMarkerComingBack = true;
+        } else {
+            for (int i = 0; i < movingMarkers.size(); i++) {
+                animateMarker(movingMarkers.get(i), route.get(i), false);
+            }
+            isMarkerComingBack = false;
+        }
+    }
+
+    /**
+     * function to initialize list of route points.
+     * */
     private void initializeRouteList() {
         route.add(0, new LatLng(53.4275, 14.5518));
         route.add(1, new LatLng(53.4275, 14.5538));
@@ -164,7 +145,7 @@ public class MapActivity extends Activity implements View.OnClickListener {
             setCenterPointOfMap(new LatLng(53.4285, 14.5528));
             moveCamera(centerPoint);
 
-            //addMarkerToMap(new LatLng(53.4275, 14.5518), R.drawable.flag_red);
+            //adding markers to map
             addMarkerToMap(route.get(0), R.drawable.flag_red);
             addMarkerToMap(route.get(1), R.drawable.flag_red);
             addMarkerToMap(route.get(2), R.drawable.flag_red);
@@ -180,38 +161,6 @@ public class MapActivity extends Activity implements View.OnClickListener {
 
     private void setCenterPointOfMap(LatLng latLng) {
         this.centerPoint = latLng;
-    }
-
-    private void addCharacter(LatLng position, final int rangeOfVisibility, String name, String description) {
-        MarkerOptions characterMarkerOptions = new MarkerOptions()
-                .title(name)
-                .snippet(description)
-                .position(position)
-                .anchor(0.5f, 0.5f)
-                .flat(false)
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.character));
-        googleMap.addMarker(characterMarkerOptions);
-        final Circle circle = googleMap.addCircle(new CircleOptions()
-                .center(position)
-                .strokeWidth(4f)
-                .strokeColor(Color.BLUE)
-                .radius(rangeOfVisibility));
-
-        ValueAnimator vAnimator = new ValueAnimator();
-        vAnimator.setRepeatCount(ValueAnimator.INFINITE);
-        vAnimator.setRepeatMode(ValueAnimator.RESTART);
-        vAnimator.setIntValues(0, rangeOfVisibility);
-        vAnimator.setDuration(ANIMATION_DURATION_MS);
-        vAnimator.setEvaluator(new IntEvaluator());
-        vAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
-        vAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator valueAnimator) {
-                float animatedFraction = valueAnimator.getAnimatedFraction();
-                circle.setRadius(animatedFraction * rangeOfVisibility);
-            }
-        });
-        vAnimator.start();
     }
 
     private void moveCamera(LatLng latLng) {
@@ -239,7 +188,7 @@ public class MapActivity extends Activity implements View.OnClickListener {
         final Handler handler = new Handler();
         final long start = SystemClock.uptimeMillis();
         final LatLng startLatLng = marker.getPosition();
-        //move time [ms]
+        //single move time [ms]
         final long duration = 2000;
 
         final Interpolator interpolator = new LinearInterpolator();
@@ -268,64 +217,5 @@ public class MapActivity extends Activity implements View.OnClickListener {
                     }
                 }
         });
-    }
-
-    public void moveMarkers(final int markerPosition, final int toPosition,
-                              final boolean hideMarker) {
-        final Marker marker = movingMarkers.get(markerPosition);
-        final Handler handler = new Handler();
-        final long start = SystemClock.uptimeMillis();
-        final LatLng startLatLng = marker.getPosition();
-        //move time [ms]
-        final long duration = 2000;
-
-        final Interpolator interpolator = new LinearInterpolator();
-
-        while (isMarkerMoving) {
-            handler.post(new Runnable() {
-
-                LatLng startPos = null;
-                LatLng stopPos = null;
-                int routePosition = toPosition;
-
-                @Override
-                public void run() {
-
-                    while(isMarkerMoving) {
-
-                    startPos = marker.getPosition();
-                    stopPos = route.get(routePosition);
-
-                    long elapsed = SystemClock.uptimeMillis() - start;
-                    float t = interpolator.getInterpolation((float) elapsed
-                            / duration);
-                    double lng = t * stopPos.longitude + (1 - t)
-                            * startPos.longitude;
-                    double lat = t * stopPos.latitude + (1 - t)
-                            * startPos.latitude;
-                    marker.setPosition(new LatLng(lat, lng));
-
-                    if (t < 1.0) {
-                        // Post again 16ms later.
-                        handler.postDelayed(this, 16);
-                    } else {
-                        if (hideMarker) {
-                            marker.setVisible(false);
-                        } else {
-                            marker.setVisible(true);
-                        }
-                    }
-
-                        if (routePosition < 4) {
-                            routePosition += 1;
-                        }
-                        else {
-                            routePosition = 0;
-                        }
-                }
-
-                }
-            });
-        }
     }
 }
